4adf4ac79c06ceddf31a9be73eef6f7affb853bc
Odero54 <oderogeorge308@gmail.com>
2024-06-24 17:11:35 +0300

    project updates

--- Blog/mysite/blog/models.py
+++ Blog/mysite/blog/models.py
@@ -1,12 +1,35 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    class Status(models.TextChoices): # Enumeration class
+        # enum members and their values
+        DRAFT = 'DF', 'Draft' # names of choices
+        PUBLISHED = 'PB', 'Published' # names of choices
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) # store the date when the post was created. Date is saved automatically!
+    updated = models.DateTimeField(auto_now=True) # Stores the last date and time when the post was updated.
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title
